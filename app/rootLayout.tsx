e569ef03bb073ab6f935d7203b7c022a9b321a01
gyur1kim <qri1114@gmail.com>
2025-02-18 22:56:35 +0900

    feat: 쿠키를 이용하여 로그인 기능 구현

--- app/rootLayout.tsx
+++ app/rootLayout.tsx
@@ -1,12 +1,17 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
+import LoggedInScreen from './screens/LoggedInScreen';
+import { useAuthStore } from './store/useAuthStore';
 
 const Stack = createNativeStackNavigator();
 
 export default function RootLayout() {
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+
   return (
-    <Stack.Navigator initialRouteName="Login">
+    <Stack.Navigator initialRouteName={isLoggedIn ? 'LoggedIn' : 'Login'}>
       <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="LoggedIn" component={LoggedInScreen} />
     </Stack.Navigator>
   );
 }
---
+++ app/screens/LoggedInScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function LoggedInScreen() {
+  return (
+    <View>
+      <Text>로그인 되고 나올 화면</Text>
+    </View>
+  );
+}
