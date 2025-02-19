f5b8ca940e27d889bf1be1b72edf49967d7e7129
gyur1kim <qri1114@gmail.com>
2025-02-19 16:10:54 +0900

    style: 파일 이름 변경

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export default function LoggedInScreen() {
+export default function HomeScreen() {
   return (
     <View>
       <Text>로그인 되고 나올 화면</Text>
